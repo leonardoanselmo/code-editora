a0106a327d6853b94fb759f5cf8da31ee86fb220
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-17 00:20:48 -0300

    Criacao do seed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.3
+                    Laravel
                 </div>
 
                 <div class="links">
