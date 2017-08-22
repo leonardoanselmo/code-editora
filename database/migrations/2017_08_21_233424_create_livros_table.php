c5207d1e98f9243c9ea1f91aa8ef1ab0d987ecb4
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-21 22:00:05 -0300

    Migration para o banco de dados criando a tablea livros

---
+++ database/migrations/2017_08_21_233424_create_livros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLivrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->notnull();
+            $table->string('subtitle')->notnull();
+            $table->float('price')->notnull();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+}
