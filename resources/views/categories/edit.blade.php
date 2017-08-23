a46dc97e1e7d331c3626ce8f107401607e5e428a
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-08-23 19:56:40 -0300

    Criação das macros e mensagens de erro

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -8,15 +8,17 @@
             {!! Form::model($category, [
                 'route' => ['categories.update', 'category' => $category->id 
                 ], 'class' => 'form', 'method' => 'PUT' ]) !!}
-                
-            <div class="form-group">
-                {!! Form::label('name', 'Nome') !!}
-                {!! Form::text('name', null, ['class' => 'form-control']) !!}
-            </div>  
 
-            <div class="form-group">
-                {!! Form::submit('Salvar categoria', ['class' => 'btn btn-primary']) !!}
-            </div>
+                {!! Html::openFormGroup('name', $errors) !!}
+                    {!! Form::label('name', 'Nome', ['class' => 'control-label']) !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                    {!! Form::error('name', $errors) !!}
+                {!! Html::closeFormGroup() !!} 
+
+
+                {!! Html::openFormGroup() !!}
+                    {!! Form::submit('Salvar categoria', ['class' => 'btn btn-primary']) !!}
+                {!! Html::closeFormGroup() !!}
 
             {!! Form::close() !!}
         </div>
