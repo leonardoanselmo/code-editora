7aa00e31b90f6cab53378e745a0e42e47b86572d
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-04 20:29:56 -0300

    migrate do relacionamento livros_users

---
+++ database/migrations/2017_09_04_230731_livros_add_users_id.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LivrosAddUsersId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('livros', function (Blueprint $table) {
+            $table->integer('users_id')->unsigned();
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+}
