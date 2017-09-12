66f520b1600a75ae9661ba278ddb6e1d6abe1c07
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-12 08:23:14 -0300

    ajuste no provider

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -25,8 +25,8 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        // $categories = $this->repository->paginate(10);
-        $categories = Category::query()->paginate(10);
+        $categories = $this->repository->paginate(10);
+        // $categories = Category::query()->paginate(10);
         return view('categories.index', compact('categories'));
     }
 
--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class,
 
     ],
     /*
