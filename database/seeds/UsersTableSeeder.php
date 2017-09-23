de94350dab06fe2e23556301c670516d3c608de4
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-22 21:35:16 -0300

    modificações nas seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\User::class, 1)->create([
+        factory(\CodePub\Models\User::class, 1)->create([
             'email' => 'admin@editora.com'
         ]);
     }
